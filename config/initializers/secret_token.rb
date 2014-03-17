515836ac5407036cb977763cdf492709a7afb1e0
Joe Love <joe.love@which.co.uk>
2014-03-17 16:33:42 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsGaTests::Application.config.secret_key_base = 'f5ea00f6ea4ea72818bb746a60256dea8c5ea0e6efaff467bb91a6048aa4b07555f5e67e22a34299b47f8df4d7e23646920a6a699c1f815a9b7dcbea18747184'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsGaTests::Application.config.session_store :cookie_store, key: '_rails_ga_tests_session'
